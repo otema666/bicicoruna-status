e9f9acccc1fb7ad89feb66caf2ccf6dab9279339
otema <otema000@gmail.com>
2023-10-29 18:47:46 +0100

    add colors

--- script.js
+++ script.js
@@ -50,41 +50,47 @@ function actualizarTabla(){
 }
 
   
-function mostrar_tabla(objeto_estaciones){
+function mostrar_tabla(objeto_estaciones) {
   const tableBody = document.querySelector("#data2 tbody");
-  
-  objeto_estaciones.forEach(objeto_estaciones =>{
+
+  objeto_estaciones.forEach((objeto_estaciones) => {
     var num_total_docks = objeto_estaciones.num_bikes_available + objeto_estaciones.num_docks_available;
-    var porcentaje_bicis = "\t(" + (objeto_estaciones.num_bikes_available / num_total_docks * 100).toFixed(2) + " %)";
-    var porcentaje_bicis_formato = objeto_estaciones.num_bikes_available + "/" + num_total_docks + "<span style='color:grey;'>" + porcentaje_bicis + "</span>"; //casera by otema666
+    var porcentaje_bicis = objeto_estaciones.num_bikes_available / num_total_docks * 100;
+    var porcentaje_bicis_str = "\t(" + porcentaje_bicis.toFixed(2) + " %)";
+    var porcentaje_bicis_formato = objeto_estaciones.num_bikes_available + "/" + num_total_docks + "<span style='color:grey;'>" + porcentaje_bicis_str + "</span>";
+
+    var hue = (porcentaje_bicis / 100) * 120;
+    var backgroundColor = `hsla(${hue}, 100%, 50%, 0.7)`; // 0.8 es la opacidad
     
     // para el unix
     let unix_timestamp = objeto_estaciones.last_reported;
     var date = new Date(unix_timestamp * 1000);
-    var reported_time = date.toLocaleTimeString("it-IT")
+    var reported_time = date.toLocaleTimeString("it-IT");
 
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>${objeto_estaciones.name}</td>
+      <td><strong>${objeto_estaciones.name}</strong></td>
       <td>${objeto_estaciones.num_bikes_available === 0 ? "No" : "Sí"}</td>
       <td>${objeto_estaciones.num_bikes_available_types.ebike}</td>
       <td>${objeto_estaciones.num_bikes_available_types.mechanical}</td>
-      <td>${porcentaje_bicis_formato}</td>
+      <td style="background-color: ${backgroundColor}">${porcentaje_bicis_formato}</td>
       <td>${objeto_estaciones.is_renting ? "Sí" : "No"}</td>
       <td>${objeto_estaciones.num_bikes_disabled}</td>
       <td>${objeto_estaciones.num_docks_disabled}</td>
       <td>${objeto_estaciones.status}</td>
       <td>${reported_time}</td>
     `;
-    
-  if (objeto_estaciones.num_bikes_available !== 0) {
-    row.querySelector('td:nth-child(2)').classList.add('bikes-yes');
-  } else {
-    row.querySelector('td:nth-child(2)').classList.add('bikes-no');
-  }
-  
-  tableBody.appendChild(row);
-})};
+
+    if (objeto_estaciones.num_bikes_available !== 0) {
+      row.querySelector('td:nth-child(2)').classList.add('bikes-yes');
+    } else {
+      row.querySelector('td:nth-child(2)').classList.add('bikes-no');
+    }
+
+    tableBody.appendChild(row);
+  });
+}
+
 
 actualizarTabla() // Primer frame
 setInterval(actualizarTabla, 5500); // Actualizacion de la tabla (loop)
